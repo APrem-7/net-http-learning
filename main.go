e1dd1da20ad877c6baf45e3bbf6b4ef3f460d616
APrem-7 <ap7leo@gmail.com>
2026-04-04 01:55:40 +0530

    feat: add HTTP server struct and implement ServeHTTP
    Introduce a server struct to encapsulate the address and handler logic.
    This improves code organization by separating concerns, making the main
    function cleaner and the server more testable and maintainable.

--- main.go
+++ main.go
@@ -2,10 +2,17 @@ package main
 
 import "net/http"
 
-type a
+type server struct {
+	addr string
+}
 
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write(([]byte("Hello From the server")))
+}
 func main() {
-
-	http.ListenAndServe(addr,)
+	s := &server{
+		addr: ":8080",
+	}
+	http.ListenAndServe(s.addr, s)
 
 }
