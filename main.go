e1f3178d55b1ddf163eb55a7fce394f5a416021f
APrem-7 <ap7leo@gmail.com>
2026-04-04 02:10:25 +0530

    adding mux and removing the switch stamtents to use the mux style switching instead

--- main.go
+++ main.go
@@ -10,31 +10,21 @@ type api struct {
 }
 
 func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-
-	case http.MethodGet:
-		switch r.URL.Path {
-		case "/":
-			w.Write(([]byte("Hello From the index page")))
-			return
-		case "/users":
-			w.Write(([]byte("Hello From the users page")))
-			return
-		case "posts":
-			w.Write(([]byte("Hello From the posts page")))
-			return
-		}
-	default:
-		w.Write(([]byte("Hello From the 404 page")))
-	}
+	w.Write(([]byte("Hello From the index page")))
 }
+
 func main() {
 	api := &api{
 		addr: ":8080",
 	}
+	mux := http.NewServeMux()
 	s := &http.Server{
-		Addr: api.addr,
+		Addr:    api.addr,
+		Handler: mux,
 	}
+
+	mux.HandleFunc("GET /users", api.getUsersHandler) //still not implenented
+	mux.HandleFunc("GET /posts", api.getPostsHandler) //still not implemetned
 	if err := http.ListenAndServe(s.Addr, api); err != nil {
 		log.Fatal("error")
 	}
