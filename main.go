a3da2250abe0d589f33a6e149c3428213cd266e1
APrem-7 <ap7leo@gmail.com>
2026-04-04 02:06:02 +0530

    refactor: rename server to api and use explicit http.Server
    This improves clarity by using a more descriptive type name and
    explicitly constructing an http.Server instance for better
    readability and future extensibility.

--- main.go
+++ main.go
@@ -5,18 +5,21 @@ import (
 	"net/http"
 )
 
-type server struct {
+type api struct {
 	addr string
 }
 
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Write(([]byte("Hello From the server")))
 }
 func main() {
-	s := &server{
+	api := &api{
 		addr: ":8080",
 	}
-	if err := http.ListenAndServe(s.addr, s); err != nil {
+	s := &http.Server{
+		Addr: api.addr,
+	}
+	if err := http.ListenAndServe(s.Addr, api); err != nil {
 		log.Fatal("error")
 	}
 
