1a55a5f630c98a66409ab0d932004c7aeac91e0a
APrem-7 <ap7leo@gmail.com>
2026-04-04 03:01:52 +0530

    Add the API for the posting Users into the slice

--- api.go
+++ api.go
@@ -25,3 +25,22 @@ func (a *api) getUsersHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.WriteHeader(http.StatusOK)
 }
+func (a *api) getPostsHandler(w http.ResponseWriter, r *http.Request) {
+	var payload User
+	w.Header().Set("Content-Type", "application/json")
+	//now we are posting into the users slice so
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	u := User{
+		first_name: payload.first_name,
+		last_name:  payload.last_name,
+	}
+
+	users.append(u)
+
+	w.WriteHeader(http.StatusOK)
+}
